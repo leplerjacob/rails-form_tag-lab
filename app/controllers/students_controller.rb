8719f5a88a844e098cc2f331638cd8df3b9c072a
Jacob Lepler <lepler.jacob@gmail.com>
2021-02-06 14:35:42 -0500

    commit tree bd86df9f48ddfab6e6b065faf5ab316435e27fd5

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,9 +3,24 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
-
+  
   def show
     @student = Student.find(params[:id])
   end
+  
+  def new
+    @student = Student.new
+  end
+
+  def create
+    Student.create(
+      first_name: params[:student][:first_name],
+      last_name: params[:student][:last_name]
+    )
+    # @student = params[:student]
+
+    redirect_to new_student_path
+  end
+
 
 end
